fc379b254ab9e54fad2c790c115845870a4a70c9
Ricardo Carvalho <ricardo.carvalho@ciandt.com>
2024-04-26 21:44:34 -0300

    Single name scenario

--- app/src/test/java/com/example/myapplication/AuthorNameFormatterUnitTest.kt
+++ app/src/test/java/com/example/myapplication/AuthorNameFormatterUnitTest.kt
@@ -18,12 +18,18 @@ class AuthorNameFormatterUnitTest {
         assertEquals("", formatter.formatted(" "))
     }
 
+    @Test
+    fun testSingleNameShouldBeUppercase() {
+        val formattedName = formatter.formatted("Carvalho")
+        assertEquals("CARVALHO", formattedName)
+    }
+
     // _TDD: surname should be the last name part and be uppercase and comma separated
         // _TDD: empty
         // _TDD: blank
         // TDD: all underscore
         // TDD: all upperscore
-    // TDD: single name should be uppercase
+    // _TDD: single name should be uppercase
     // TDD: compound surname if contains at least three parts and last is kinship
     // TDD: surname prepositions should be lowercase
 }
