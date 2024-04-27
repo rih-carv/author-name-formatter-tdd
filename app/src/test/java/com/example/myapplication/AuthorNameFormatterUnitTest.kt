d05da3c69a5838b7d901cfcb5dc7f6f0dd64e666
Ricardo Carvalho <ricardo.carvalho@ciandt.com>
2024-04-26 21:33:37 -0300

    Initial TDD to do list

---
+++ app/src/test/java/com/example/myapplication/AuthorNameFormatterUnitTest.kt
@@ -0,0 +1,8 @@
+package com.example.myapplication
+
+class AuthorNameFormatterUnitTest {
+    // TDD: surname should be the last name part and be uppercase and comma separated
+    // TDD: single name should be uppercase
+    // TDD: compound surname if contains at least three parts and last is kinship
+    // TDD: surname prepositions should be lowercase
+}
