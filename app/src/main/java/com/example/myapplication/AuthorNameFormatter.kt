1c3129b0fd8e6b2ef180cb536d8b173cd7ad6da5
Ricardo Carvalho <ricardo.carvalho@ciandt.com>
2024-04-29 12:07:57 -0300

    Kinship like logic refactoring

--- app/src/main/java/com/example/myapplication/AuthorNameFormatter.kt
+++ app/src/main/java/com/example/myapplication/AuthorNameFormatter.kt
@@ -25,13 +25,7 @@ class AuthorNameFormatter {
         name.trim()
             .split(" ")
             .run {
-                var stopOnNext = false
-                val lastNames = takeLastWhile {
-                    !stopOnNext.also { _ ->
-                        if (count() < 3) stopOnNext = true
-                        if (it.lowercase() !in KINSHIP_LIKE) stopOnNext = true
-                    }
-                }
+                val lastNames = takeLast(lastNameAmountConsideringKinshipLike())
                 listOfNotNull(
                     lastNames
                         .joinToString(" ")
@@ -52,4 +46,11 @@ class AuthorNameFormatter {
                 if (index == 0) namePart.uppercase()
                 else namePart
             }.joinToString()
+
+    private fun List<String>.lastNameAmountConsideringKinshipLike() =
+        if (count() < 3) 1
+        else foldRight(1) { nameWord, amount ->
+            if (nameWord.lowercase() in KINSHIP_LIKE) amount + 1
+            else return amount
+        }
 }
